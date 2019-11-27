44bdcf7947a908f801bd5865d2afcc4f341ff73a
gamedesignbrasil <thiago@spellbook.com.br>
2019-11-27 13:42:19 -0300

    Atualizando auth de login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,23 +2,23 @@ import axios from "axios";
 import { push } from "connected-react-router";
 import { routes } from "../containers/Router";
 
-// export const login = (email, password) => async dispatch => {
-//     try {
-//         const response = await axios.post(
-//             "https://us-central1-missao-newton.cloudfunctions.net/rappi4/login",
-//             {
-//                 email,
-//                 password
-//             }
-//         );
-//         console.log("Login feito com sucesso!")
-//         window.localStorage.setItem("token", response.data.token);
-//         // dispatch(push(routes.feed));
-//     } catch (e) {
-//         window.alert(e.message)
-//         console.log("Erro no login")
-//     }
-// };
+export const login = (email, password) => async dispatch => {
+    try {
+        const response = await axios.post(
+            "https://us-central1-missao-newton.cloudfunctions.net/rappi4/login",
+            {
+                email,
+                password
+            }
+        );
+        console.log("Login feito com sucesso!")
+        window.localStorage.setItem("token", response.data.token);
+        dispatch(push(routes.feedpage));
+    } catch (e) {
+        window.alert(e.message)
+        console.log("Erro no login")
+    }
+};
 
 export const signUp = (name, email, cpf, password) => async dispatch => {
     try {
