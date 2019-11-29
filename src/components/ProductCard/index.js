b620617e2a98b3269ea2068434c998d03152c585
d-uartepereira <daviduarte973@gmail.com>
2019-11-29 11:41:03 -0300

    Finished mocked restaturants page.

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import { StyledCardContainer, StyledImg, StyledCardContent, StyledTitleSpan, StyledContentSpan, StyledPriceSpan, StyledButton, StyledCounter } from './styled';
+import { StyledCardContainer, StyledImg, StyledCardContent, StyledTitleSpan, StyledContentSpan, StyledPriceSpan, StyledButton, StyledCounter, StyledImgWrapper } from './styled';
 
 
 export default function ProductCard(props) {
 
     return (
         <StyledCardContainer>
-            <StyledImg src={props.product.photoUrl} />
+            <StyledImgWrapper>
+                <StyledImg src={props.product.photoUrl} />
+            </StyledImgWrapper>
             <StyledCardContent>
                 <StyledTitleSpan>{props.product.name}</StyledTitleSpan>
                 <StyledContentSpan>{props.product.description}</StyledContentSpan>
-                <StyledPriceSpan>R$ 0{props.product.price},00</StyledPriceSpan>
+                <StyledPriceSpan>R$ {props.product.price},00</StyledPriceSpan>
             </StyledCardContent>
             <StyledCounter>2</StyledCounter>
-            <StyledButton>adicionar</StyledButton>
+            <StyledButton onClick={props.onClickAddButton}>adicionar</StyledButton>
         </StyledCardContainer>
     )
 }
--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route } from "react-router-dom";
 import Login from "../Login/Login";
-import SignUp from "../SignUp/SignUp";
+import SignUp from "../SignUp/signup";
 import Address from "../Address/Address";
 import { Carrinho } from "../Carrinho/Carrinho";
 import ProductCard from "../../components/ProductCard";
 import FeedPage from "../FeedPage";
 import PerfilPage from "../PerfilPage";
+import DetailedRestaurantPage from "../DetailedRestaurantPage";
 
 export const routes = {
   root: "/",
@@ -16,19 +17,20 @@ export const routes = {
   cart: "/cart",
   perfil: "/perfil",
   feed: "/feed",
+  restaurant: "/restaurant"
 };
 
 function Router(props) {
   return (
     <ConnectedRouter history={props.history}>
       <Switch>
-        <Route exact path={routes.root} component={Login} />
+        {/* <Route exact path={routes.root} component={Login} /> */}
         <Route exact path={routes.signup} component={SignUp} />
         <Route exact path={routes.address} component={Address} />
         <Route exact path={routes.cart} component={Carrinho} />
         <Route exact path={routes.feed} component={FeedPage} />
         <Route exact path={routes.perfil} component={PerfilPage} />
-        <Route exact path={routes.root} component={ProductCard} />
+        <Route exact path={routes.root} component={DetailedRestaurantPage} />
       </Switch>
     </ConnectedRouter>
   );
